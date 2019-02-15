d06388c08676fe26c712cedc0c4cd4e1cfa110ea
Jarosław Bagnicki <jarek@bagnicki.net>
2019-02-15 13:25:13 +0100

    Fix user link url in User component

--- src/components/User/index.js
+++ src/components/User/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './User.styles.module.scss';
 
-export const User = ({user: {login, avatar_url, url}}) => (
+export const User = ({user: {login, avatar_url, html_url}}) => (
   <div className={styles.container}>
     <img src={avatar_url} className={styles.avatar}/>
-    <a href={url} target="_blank" rel="noopener noreferrer" className={styles.link}>{login}</a>
+    <a href={html_url} target="_blank" rel="noopener noreferrer" className={styles.link}>{login}</a>
   </div>
 );
 
@@ -13,6 +13,6 @@ User.propTypes = {
   user: PropTypes.shape({
     login: PropTypes.string.isRequired,
     avatar_url: PropTypes.string.isRequired,
-    url: PropTypes.string.isRequired
+    html_url: PropTypes.string.isRequired
   })
 };
