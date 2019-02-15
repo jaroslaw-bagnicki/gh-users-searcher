51d741de682b96017834e9e66a03001c83ce0ce0
Jarosław Bagnicki <jarek@bagnicki.net>
2019-02-15 11:45:07 +0100

    Add User component

--- src/components/User/index.js
+++ src/components/User/index.js
@@ -1,5 +1,17 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-export const User = () => (
-  '-- user component --'
+export const User = ({user: {login, avatar_url, url}}) => (
+  <div>
+    <img src={avatar_url} />
+    <a href={url} target="_blank" rel="noopener noreferrer">{login}</a>
+  </div>
 );
+
+User.propTypes = {
+  user: PropTypes.shape({
+    login: PropTypes.string.isRequired,
+    avatar_url: PropTypes.string.isRequired,
+    url: PropTypes.string.isRequired
+  })
+};
