7f3f5de6fe0464150326053b16157d3567398198
Jarosław Bagnicki <jarek@bagnicki.net>
2019-02-15 10:28:12 +0100

    Add App container and UserList component scaffold

--- src/App.js
+++ src/App.js
@@ -1,5 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { UserList } from './components/UserList';
 
-export const App = () => (
-  'Hello world!'
-)
+export class App extends Component {
+  state = {
+    searchText: '',
+    users: []
+  };
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleFormSubmit}>
+          <label htmlFor="search">Search by user name</label>
+          <input
+            type="search"
+            id="search"
+            onChange={this.handleInputChange}
+            value={this.state.searchText} />
+        </form>
+        <UserList users={this.state.users} />
+      </div>
+    );
+  }
+
+  handleInputChange = (e) => {
+    console.log('handleInputChange()');
+    this.setState({
+      searchText: e.target.value
+    });
+  }
+
+  handleFormSubmit = (e) => {
+    e.preventDefault();
+    console.log('handleFormSubmit()');
+  }
+}
