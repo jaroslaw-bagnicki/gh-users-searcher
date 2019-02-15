b2b96119f746b1bbe37af49a9e9a70f9cde5b3a2
Jarosław Bagnicki <jarek@bagnicki.net>
2019-02-15 11:29:40 +0100

    Add UserList component

--- src/components/UserList/index.js
+++ src/components/UserList/index.js
@@ -1,5 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
-export const UserList = () => (
-  '-- UserList component --'
-);
+import { User } from '../User';
+
+export class  UserList extends Component {
+  static propTypes = {
+    users: PropTypes.arrayOf(PropTypes.object).isRequired,
+    totalUsers: PropTypes.number.isRequired
+  }
+
+  render() {
+    const { users, totalUsers } = this.props;
+    return (
+      <div>
+        <p>Found {totalUsers} users</p>
+        <ul>
+          {users.map(user => (
+            <li key={user.id}>
+              <User user={user} />
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
